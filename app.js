f17237d1180da8784046be12b1b2bb5278904ff5
Arashi Funsho <unorthodox@cyberdude.com>
2022-02-04 16:30:02 -0700

    Added some express routes

--- app.js
+++ app.js
@@ -1,17 +1,71 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
 const port = 3000;
+const simpleToursFile = `${__dirname}/dev-data/data/tours-simple.json`;
 
+app.use(express.json()); //this middleware helps us to modify the request data
 //ROUTES
+// app.get('/', (req, res) => {
+//   res.json({ message: 'Hello from the Server side', app: 'Natours' });
+// });
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Hello from the Server side', app: 'Natours' });
+// app.post('/', (req, res) => {
+//   res.send('You can post to this endpoint');
+// });
+
+const tours = JSON.parse(fs.readFileSync(simpleToursFile));
+
+//get the tours json
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
-app.post('/', (req, res) => {
-  res.send('You can post to this endpoint');
+//get a tour by it's id
+
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1; //parse the id string to number via multiplying by 1
+
+  const tour = tours.find((element) => element.id === id);
+  //if (id > tours.length) {
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid Tour Id',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
+
+//save a tour
+app.post('/api/v1/tours', (req, res) => {
+  //console.log(req.body);
+  const newID = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newID }, req.body);
+  tours.push(newTour);
+
+  fs.writeFile(simpleToursFile, JSON.stringify(tours), (err) => {
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  });
 });
 
 //START THE APP
