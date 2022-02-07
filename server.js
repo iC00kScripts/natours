1deb9b5cd653b61daa3ba036ffccf91e140fd47c
Arashi Funsho <unorthodox@cyberdude.com>
2022-02-07 16:48:57 -0700

    Added Mongoose drivers and initialized the database connection

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: '.env' }); //load environment variables
+
+//initialize the database using the environment variables.
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DB_PASSWORD);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then((conn) => {
+    console.log('DB Connection successful!');
+  });
 
 //console.log(process.env);
 
