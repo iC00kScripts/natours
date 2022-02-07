e4a30fd3cdf6d2284516cc2067c587ea7636ce0e
Arashi Funsho <unorthodox@cyberdude.com>
2022-02-06 18:52:28 -0700

    added dotenv package and made minor changes to use env variables

--- app.js
+++ app.js
@@ -8,8 +8,12 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 //middleware
-app.use(morgan('dev')); //using the morgan logging middleware
+if (process.env.NODE_ENV !== 'production') {
+  app.use(morgan('dev')); //using the morgan logging middleware
+}
+
 app.use(express.json()); //this middleware helps us to modify the request data
+app.use(express.static(`${__dirname}/public`)); // middleware to help serve static files in public folder
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString(); //add the current time to the requests using middleware
--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const app = require('./app');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: '.env' });
+
+console.log(process.env);
 
 //START THE SERVER
-const port = 3000;
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
