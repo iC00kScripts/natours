1bf03e19ad1ae3d5a2584e844c9cde8ce82d803c
Arashi Funsho <unorthodox@cyberdude.com>
2022-02-06 19:03:55 -0700

    added dev and prod scripts to packages.json

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const app = require('./app');
 const dotenv = require('dotenv');
 
-dotenv.config({ path: '.env' });
+dotenv.config({ path: '.env' }); //load environment variables
 
-console.log(process.env);
+//console.log(process.env);
 
 //START THE SERVER
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8000; //read port from environment if available else use defined port
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
