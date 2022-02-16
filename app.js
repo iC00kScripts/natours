4aa8e7c0ef3bd0bbb0e87fa5ffed5b16c86bf6d3
Arashi Funsho <unorthodox@cyberdude.com>
2022-02-16 09:38:41 -0700

    Added error handling middleware

--- app.js
+++ app.js
@@ -24,4 +24,27 @@ app.use((req, res, next) => {
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/tours', tourRouter);
 
+//handling exception for other routes not declared above
+app.all('*', (req, res, next) => {
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find ${req.originalUrl} on this server`
+  // });
+  const err = new Error(`Can't find ${req.originalUrl} on this server`);
+  err.status = 'fail';
+  err.statusCode = 404;
+  next(err);// this works because express only expects an argumment in the next function if there's an error.
+});
+
+//error handling middleware
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode ?? 500;
+  err.status = err.status ?? 'error';
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message
+  });
+});
+
+
 module.exports = app;
