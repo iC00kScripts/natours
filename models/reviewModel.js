fb14f28a6dacd2f6a822151f2aef876a3880c405
Funsho Arashi <unorthodox@cyberdude.com>
2022-03-10 13:54:12 -0700

    Added update of review stats on Tour during review CUD operations

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Tour = require('./tourModel');
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -45,6 +46,49 @@ reviewSchema.pre(/^find/, function (next) {
     select: 'name photo',
   }).select('-__v');
   next();
+});
+
+//creating a static method to calculate AverageRatings and update Tour when a new review is created
+reviewSchema.statics.calcAverageRatings = async function (tourId) {
+  const stats = await this.aggregate([
+    //'this' points to the Model in this case
+    {
+      $match: { tour: tourId }, //select the tour that the review belongs to
+    },
+    {
+      $group: {
+        _id: '$tour',
+        nRating: { $sum: 1 }, //sum up all the reviews matching the tourId
+        avgRating: { $avg: '$rating' }, //calculate the average of the ratings on each reviews
+      },
+    },
+  ]);
+
+  //check if reviews exist on the tour and then...
+  if (stats.length > 0) {
+    //update the tour with the newly calculated aggregates
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: stats[0].nRating, //since stats is an array, we obtain the properties from the first position
+      ratingsAverage: stats[0].avgRating,
+    });
+  } else {
+    //restore to default
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: 0, //since stats is an array, we obtain the properties from the first position
+      ratingsAverage: 4.5,
+    });
+  }
+};
+
+//POST query middleware calling the function to recalculate averages after a new review is created.
+reviewSchema.post('save', function () {
+  //'this' points to the current review document being created
+  this.constructor.calcAverageRatings(this.tour);
+});
+
+reviewSchema.post(/^findOneAnd/, async function (doc) {
+  //receive the newly updated document
+  if (doc) Review.calcAverageRatings(doc.tour); //load the static method on the model and update the tour/review stats
 });
 
 const Review = mongoose.model('Review', reviewSchema);
