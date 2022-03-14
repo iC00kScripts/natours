ae81f66ff6a4c987377dccb62d0cc2d8084e676d
Funsho Arashi <unorthodox@cyberdude.com>
2022-03-14 14:46:06 -0600

    refactored the routes adding authorization and authentication middlewares independently

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -15,14 +15,21 @@ router
   .route('/top-5-cheap')
   .get(tourController.aliasTopTours, tourController.getAllTours);
 
-router.route('/').get(tourController.getAllTours).post(
-  authController.protect, //protect this endpoint
-  authController.restrictTo('admin', 'lead-guide'), //only admin and lead guides can create new tours
-  tourController.createTour
-); // to use a middleware on this path alone, use something like.post(tourController.checkBody, tourController.createTour);
-
 //get the stats using its own route.
 router.route('/tour-stats').get(tourController.getTourStats);
+
+//get all tours within :distance radius from :latlng. distance measures in :unit (km/mi)
+router
+  .route('/tours-within/:distance/center/:latlng/unit/:unit')
+  .get(tourController.getToursWithin);
+
+router.route('/distances/:latlng/unit/:unit').get(tourController.getDistances);
+//the route's home is /api/v1/tours therefore no need to include that in the route path again
+router.route('/:id').get(tourController.getTour);
+router.route('/').get(tourController.getAllTours);
+
+//user must be properly authenticated to use all endpoints below
+router.use(authController.protect);
 
 //get monthly plan by year.
 router
@@ -32,25 +39,16 @@ router
     tourController.getMonthlyPlan
   );
 
-//get all tours within :distance radius from :latlng. distance measures in :unit (km/mi)
-router
-  .route('/tours-within/:distance/center/:latlng/unit/:unit')
-  .get(tourController.getToursWithin);
+//All endpoints below are restricted to admin and lead-guide
+router.use(authController.restrictTo('admin', 'lead-guide'));
+
+router.route('/').post(tourController.createTour); //only admin and lead guides can create new tours
 
 //the route's home is /api/v1/tours therefore no need to include that in the route path again
 router
   .route('/:id')
-  .get(tourController.getTour)
-  .patch(
-    authController.protect,
-    authController.restrictTo('admin', 'lead-guide'),
-    tourController.updateTour
-  )
-  .delete(
-    authController.protect, //protect this endpoint
-    authController.restrictTo('admin', 'lead-guide'), //adding user authorization middleware
-    tourController.deleteTour
-  );
+  .patch(tourController.updateTour)
+  .delete(tourController.deleteTour);
 
 //export the file as a module, so it can be imported into another js file
 module.exports = router;
