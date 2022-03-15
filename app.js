83a4c74003a67cf8de6c1409d8220f1dc4696e04
Funsho Arashi <unorthodox@cyberdude.com>
2022-03-15 16:29:39 -0600

    Creating the serverside rendered website

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
@@ -12,10 +13,16 @@ const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
 
-//global middleware
+app.set('view engine', 'pug'); //defining the view engine
+app.set('views', path.join(__dirname, 'views')); //setting the directory for our views
+
+//global middlewares
+app.use(express.static(path.join(__dirname, 'public'))); // middleware to help serve static files in public folder
+
 app.use(helmet()); //added middleware to set security headers
 
 if (process.env.NODE_ENV !== 'production') {
@@ -54,14 +61,13 @@ app.use(
   })
 ); //package: hpp
 
-app.use(express.static(`${__dirname}/public`)); // middleware to help serve static files in public folder
-
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString(); //add the current time to the requests using middleware
   next();
 });
 
 //mount the routers, defining their home path
+app.use('/', viewRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/reviews', reviewRouter);
---
+++ controllers/viewsController.js
@@ -0,0 +1,27 @@
+const Tour = require('../models/tourModel');
+const catchAsync = require('./../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  // get all tours from collection
+  const tours = await Tour.find();
+  //build the template
+  //render template using tours data
+  res.status(200).render('overview', {
+    title: 'All Tours',
+    tours, //passing the tour data into the overview template for rendering
+  });
+});
+
+exports.getTour = catchAsync(async (req, res, next) => {
+  //get data for the requested tour (including reviews and tour guides)
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
+  });
+  //build the template
+  //render the template using the tour data
+  res.status(200).render('tour', {
+    title: tour.name,
+    tour: tour,
+  });
+});
