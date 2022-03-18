06825f4db70cdf2fd7bfd6bd4d502710353f9b83
Funsho Arashi <unorthodox@cyberdude.com>
2022-03-18 14:10:18 -0600

    implementing the error page

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,11 +1,11 @@
 const AppError = require('./../utils/appError');
 
-const handleCastErrorDB = err => {
+const handleCastErrorDB = (err) => {
   const message = `Invalid ${err.path}: ${err.value}`;
   return new AppError(message, 400);
 };
 
-const handleDuplicateFieldsDB = err => {
+const handleDuplicateFieldsDB = (err) => {
   const key = Object.keys(err.keyValue).join('');
   const message = `The key '${key}' has duplicate value of '${err.keyValue[key]}'`;
   return new AppError(message, 400);
@@ -15,61 +15,68 @@ const handleDuplicateFieldsDB = err => {
   //return new AppError(message, 400);
 };
 
-const handleValidationError = err => {
+const handleValidationError = (err) => {
   //we can also extract the message by looping the errors object: Object.values(err.errors).map(el => el.message)
   return new AppError(err.message.replace(',', '.'), 400);
 };
 
-const handleJWTError = () => new AppError('Invalid token, Please login again', 401);
-const handleTokenExpiredError = () => new AppError('Token has expired Please login again', 403);
+const handleJWTError = () =>
+  new AppError('Invalid token, Please login again', 401);
+const handleTokenExpiredError = () =>
+  new AppError('Token has expired Please login again', 403);
 
-const sendErrorDev = (err, res) => {
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-    statusCode: err.statusCode,
-    stack: err.stack,
-    error: err
+const sendErrorDev = (err, req, res) => {
+  //API
+  if (req.originalUrl.startsWith('/api')) {
+    return res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+      statusCode: err.statusCode,
+      stack: err.stack,
+      error: err,
+    });
+  }
+  console.error('ERROR::', err);
+  //Rendered Website
+  res.status(err.statusCode).render('error', {
+    title: 'Something went wrong!',
+    msg: `Error Message: ${err.message}\nError Status: ${err.status}\nError Statuscode: ${err.statusCode}\nError Stack: ${err.stack}`,
   });
 };
 
-const sendErrorProd = (err, res) => {
-  //is operational error, send to client
-  if (err.isOperational) {
-    res.status(err.statusCode).json({
-      status: err.status,
-      message: err.message
-    });
-  } else {
-    //programming or unknown error: don't leak details to the client but log to console and send generic message
-    console.error('ERROR::', err);
+const sendErrorProd = (err, req, res) => {
+  const message = err.isOperational ? err.message : 'Please try again later!';
 
-    res.status(500).json({
-      status: 'error',
-      message: 'Something went very wrong!'
+  if (req.originalUrl.startsWith('/api')) {
+    //is operational error, send to client
+    return res.status(err.statusCode).json({
+      status: err.status,
+      message,
     });
   }
 
+  console.error('ERROR::', err);
+  res.status(err.statusCode).render('error', {
+    title: 'Something went wrong!',
+    msg: message,
+  });
 };
-
 
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode ?? 500;
   err.status = err.status ?? 'error';
 
-
   if (process.env.NODE_ENV === 'development') {
-    sendErrorDev(err, res);
+    sendErrorDev(err, req, res);
   } else {
-    let error = { ...err };//desctructure and make a copy of the error.
+    let error = { ...err }; //desctructure and make a copy of the error.
+    error.message = err.message;
 
-    if (err.name === 'CastError') error = handleCastErrorDB(err);
+    if (err.name === 'CastError') error = handleCastErrorDB(error);
     if (err.code === 11000) error = handleDuplicateFieldsDB(error);
     if (err.name === 'JsonWebTokenError') error = handleJWTError();
     if (err.name === 'TokenExpiredError') error = handleTokenExpiredError();
-    if (err.name === 'ValidationError') error = handleValidationError(err); //since destructuring loses some property using the original err object here to preserve the message property.
-
-    sendErrorProd(error, res);
+    if (err.name === 'ValidationError') error = handleValidationError(error);
+    sendErrorProd(error, req, res);
   }
-
 };
