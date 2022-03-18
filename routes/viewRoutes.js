e7824f2f3fda17d2466fd439364d0439b64c1057
Funsho Arashi <unorthodox@cyberdude.com>
2022-03-18 10:25:48 -0600

    added middleware that checks for logged in user

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const viewsController = require('../controllers/viewsController');
+const authController = require('../controllers/authController');
 
 const router = express.Router();
+
+router.use(authController.isLoggedIn); //use the middleware to check for logged in user in every rendered page
 
 router.get('/', viewsController.getOverview);
 router.get('/tour/:slug', viewsController.getTour);
