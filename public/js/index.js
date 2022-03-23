af4d426f6f4c1f0e4c08ad49b5cf1d55c39ae2e7
Funsho Arashi <unorthodox@cyberdude.com>
2022-03-23 15:29:20 -0600

    added complete functionality to change user password and details

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,11 @@
 import { login, logout } from './login';
 import { displayMap } from './mapbox';
-import { updateData } from './updateSettings';
+import { updateSettings } from './updateSettings';
 
 const mapBox = document.getElementById('map');
 const loginForm = document.querySelector('.form--login');
 const userDataForm = document.querySelector('.form-user-data');
+const userPswForm = document.querySelector('.form-user-settings');
 const logoutBtn = document.querySelector('.nav__el--logout');
 
 if (mapBox) {
@@ -24,10 +25,39 @@ if (loginForm) {
 if (logoutBtn) logoutBtn.addEventListener('click', logout);
 
 if (userDataForm) {
-  userDataForm.addEventListener('submit', function (e) {
+  userDataForm.addEventListener('submit', async function (e) {
+    if (!e.target.classList.contains('form-user-data')) {
+      return;
+    }
     e.preventDefault();
+    document.querySelector('.btn--save-settings').textContent = 'UPDATING...';
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
-    updateData(name, email);
+    await updateSettings('data', { name, email });
+    document.querySelector('.btn--save-settings').textContent = 'SAVE SETTINGS';
+  });
+}
+
+if (userPswForm) {
+  userPswForm.addEventListener('submit', async function (e) {
+    // guard to check if different form submitted
+    if (!e.target.classList.contains('form-user-settings')) {
+      return;
+    }
+    e.preventDefault();
+    document.querySelector('.btn--save-password').textContent = 'UPDATING...';
+    const oldPassword = document.getElementById('password-current').value;
+    const password = document.getElementById('password').value;
+    const passwordConfirm = document.getElementById('password-confirm').value;
+    await updateSettings('password', {
+      oldPassword,
+      password,
+      passwordConfirm,
+    });
+
+    document.querySelector('.btn--save-password').textContent = 'SAVE PASSWORD';
+    document.getElementById('password-current').value = '';
+    document.getElementById('password').value = '';
+    document.getElementById('password-confirm').value = '';
   });
 }
--- public/js/updateSettings.js
+++ public/js/updateSettings.js
@@ -1,23 +1,26 @@
 import { showAlert } from './alerts';
 
-export const updateData = async (name, email) => {
+//type is either password or data
+//data is the data object
+export const updateSettings = async (type, data) => {
+  const dt = { ...data };
   try {
-    const response = await fetch(
-      'http://localhost:3000/api/v1/users/updateMe',
-      {
-        method: 'PATCH',
-        headers: {
-          'content-type': 'application/json',
-        },
-        body: JSON.stringify({ name, email }),
-      }
-    );
+    let url = 'http://localhost:3000/api/v1/users/';
+    url += type === 'data' ? 'updateMe' : 'updatePassword';
+
+    let response = await fetch(url, {
+      method: 'PATCH',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify(dt),
+    });
     if (!response.ok) throw response;
-    const data = await response.json();
+    let data = await response.json();
 
     if (data.status === 'success') {
       window.location.reload(true);
-      showAlert('success', 'Updated User data successfully', 2000);
+      showAlert('success', `Updated User ${type} successfully`, 2000);
     }
   } catch (err) {
     err.text().then((errorMessage) => {
